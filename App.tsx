5f37ba1e2b8471264ada8e4e11b2e5e89d868f67
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-09-11 22:46:11 +0300

    reanimated + gesturehandler

--- App.tsx
+++ App.tsx
@@ -1,36 +1,26 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 import HomeStack from './src/router/HomeStack';
 import StatusBarManager from './src/components/StatusBarManager';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <StatusBarManager>
-        <HomeStack />
-      </StatusBarManager>
-    </NavigationContainer>
+    <GestureHandlerRootView style={styles.flex}>
+      <NavigationContainer>
+        <StatusBarManager>
+          <HomeStack />
+        </StatusBarManager>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  flex: {
+    flex: 1,
   },
 });
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import App from './App';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
