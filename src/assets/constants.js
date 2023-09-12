2a7af710237031204f6632ed3479d6619ae5ff1f
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-09-12 18:45:23 +0300

    assets, fonts

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,4 +1,6 @@
-import {Dimensions} from 'react-native';
+import {Dimensions, Platform} from 'react-native';
 
+export const isIOS = Platform.OS === 'ios';
+export const isAndroid = Platform.OS === 'android';
 export const HEIGHT = Dimensions.get('window').height;
 export const WIDTH = Dimensions.get('screen').width;
